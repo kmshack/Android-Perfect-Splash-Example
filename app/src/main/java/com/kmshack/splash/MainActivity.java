f26befa86b070a531497ca6ab28a0d79b68a8cb4
roid.and <roid.and@kakaocorp.com>
2017-04-07 19:39:28 +0900

    no message

--- app/src/main/java/com/kmshack/splash/MainActivity.java
+++ app/src/main/java/com/kmshack/splash/MainActivity.java
@@ -7,7 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
